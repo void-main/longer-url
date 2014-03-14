005f677c818b11034b79fbdc4b72f7011799c9bc
Void Main <voidmain1313113@gmail.com>
2014-03-14 08:26:07 +0800

    add redirector to handle short-url redirection

---
+++ service/redirection.rb
@@ -0,0 +1,22 @@
+require 'net/http'
+require 'uri'
+
+class Redirector
+  def fetch(uri_str, limit = 10)
+    raise ArgumentError, 'HTTP redirect too deep' if limit == 0
+
+    url = URI.parse(uri_str)
+    req = Net::HTTP::Get.new(url.path)
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = (url.scheme == "https")
+    response = http.request(req)
+
+    case response
+      when Net::HTTPSuccess     then url
+      when Net::HTTPRedirection then fetch(response['location'], limit - 1)
+      else
+        response.error!
+    end
+  end
+end
